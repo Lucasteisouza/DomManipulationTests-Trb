a64d640bc20f6f824cffd28eb894026c9aa71353
Lucasteisouza <lucasteisouza@gmail.com>
2022-08-31 17:22:01 -0300

    Requisito 5 atendido

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ const randomColor1 = document.querySelector('.random-color1')
 const randomColor2 = document.querySelector('.random-color2')
 const randomColor3 = document.querySelector('.random-color3')
 
+let storedColors = JSON.parse(localStorage.getItem('colorPalette'))
+if (storedColors != null){
+    randomColor1.style.backgroundColor = storedColors.color1;
+    randomColor2.style.backgroundColor = storedColors.color2;
+    randomColor3.style.backgroundColor = storedColors.color3;
+}
 randomizerButton.addEventListener('click', randomizeColors)
 
 function randomRGB(){
@@ -10,7 +16,6 @@ function randomRGB(){
     let randomRGBValue2 = Math.floor(Math.random() * 255)
     let randomRGBValue3 = Math.floor(Math.random() * 255)
     let RGBColor = 'rgb(' + randomRGBValue1 + ', ' + randomRGBValue2 + ', ' + randomRGBValue3 + ')';
-    console.log(RGBColor);
     return RGBColor;
 }
 
@@ -18,4 +23,16 @@ function randomizeColors() {
     randomColor1.style.backgroundColor = randomRGB();
     randomColor2.style.backgroundColor = randomRGB();
     randomColor3.style.backgroundColor = randomRGB();
+    saveColors();
+}
+
+function saveColors(){
+    let bg1 = randomColor1.style.backgroundColor;
+    let bg2 = randomColor2.style.backgroundColor;
+    let bg3 = randomColor3.style.backgroundColor;
+    let colorsObj = {'color1': bg1,
+        'color2': bg2,
+        'color3': bg3}
+    let savedBgColors = JSON.stringify(colorsObj);
+    localStorage.setItem('colorPalette', savedBgColors)
 }
